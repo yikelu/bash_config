45dd1126b400fb237b6035b7bf11043462632d47
Yike Lu <yikelu.home@gmail.com>
2014-01-28 12:34:58 -0600

    Cleaning up deploy, adding terminus
    Also removing geomview from list and enabling bitmapped fonts in the deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
+# enable bitmapped fonts
+cd /etc/fonts/conf.d/
+sudo rm /etc/fonts/conf.d/10* && sudo rm -rf 70-no-bitmaps.conf && sudo ln -s
+../conf.avail/70-yes-bitmaps.conf .
+sudo dpkg-reconfigure fontconfig
+
+# restore dpkg stuff
 sudo dpkg --clear-selections
 sudo dpkg --set-selections < list.txt
 
+# soft link the bashrc
 ln -s ~/bash_config/.bashrc ~/.bashrc
