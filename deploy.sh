74bce59a6548fdb70ea3a047d486644f5757ae7e
Yike Lu <yikelu.home@gmail.com>
2014-01-28 19:36:42 -0600

    For loop in deploy install

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 # enable bitmapped fonts
 cd /etc/fonts/conf.d/
-sudo rm /etc/fonts/conf.d/10* && sudo rm -rf 70-no-bitmaps.conf && sudo ln -s
-../conf.avail/70-yes-bitmaps.conf .
+sudo rm /etc/fonts/conf.d/10* && sudo rm -rf 70-no-bitmaps.conf && sudo ln -s ../conf.avail/70-yes-bitmaps.conf .
 sudo dpkg-reconfigure fontconfig
 
 # restore dpkg stuff
 sudo dpkg --clear-selections
 sudo dpkg --set-selections < list.txt
+for i in `awk '{print $1}' list.txt`; do sudo apt-get install -y $i; done
 
 # soft link the bashrc
 ln -s ~/bash_config/.bashrc ~/.bashrc
