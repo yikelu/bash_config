c6d171cfe6534780cafca377b962dc6ee3a8338b
Yike Lu <yikelu.home@gmail.com>
2014-02-13 11:22:27 -0600

    Adding conda stuff

--- deploy.sh
+++ deploy.sh
@@ -16,3 +16,5 @@ ln -s ~/bash_config/.vrapperrc ~/.vrapperrc
 # install Anaconda
 wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.8.0-Linux-x86_64.sh
 bash Anaconda-1.8.0-Linux-x86_64.sh
+
+# haven't figured out how to loop through the pylist and install yet
--- save_pkgs.sh
+++ save_pkgs.sh
@@ -1,1 +1,2 @@
-sudo dpkg --get-selections > list.txt
+sudo dpkg --get-selections | grep -v deinstall > list.txt
+conda list -e > pylist.txt
