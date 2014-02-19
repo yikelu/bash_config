2e2e496978a90cf12b422d6138279e6210f73487
Yike Lu <yikelu.home@gmail.com>
2014-02-19 13:11:27 -0600

    Updating list, adding ypy
    ypy is a custom Yike alias for ipython, which runs the ~/.ypy, which itself is
    just a python file. It has a bunch of imports for easy use, like pandas as pd,
    numpy as np etc.

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ for i in `awk '{print $1}' list.txt`; do sudo apt-get install -y $i; done
 ln -s ~/bash_config/.bashrc ~/.bashrc
 ln -s ~/bash_config/.gitconfig ~/.gitconfig
 ln -s ~/bash_config/.vrapperrc ~/.vrapperrc
+ln -s ~/bash_config/.ypy ~/.ypy
 
 # install Anaconda
 wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.8.0-Linux-x86_64.sh
